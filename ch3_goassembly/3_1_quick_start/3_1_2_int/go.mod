c315e171c518558e6b9b34996077aeb3d266a3e6
batreeon <1340551309@qq.com>
2022-09-10 18:02:29 +0800

    go汇编。不知道是不是因为arm cpu的原因，跑不了
    Change-Id: I8206278cb424ce28cddb578c61acd5599a567005

---
+++ ch3_goassembly/3_1_quick_start/3_1_2_int/go.mod
@@ -0,0 +1,3 @@
+module advanced-go-programming/ch3_goassembly/3_1_quick_start/3_1_2_int
+
+go 1.18
---
+++ ch3_goassembly/3_1_quick_start/3_1_2_int/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"advanced-go-programming/ch3_goassembly/3_1_quick_start/3_1_2_int/pkg"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(pkg.Id)
+}
